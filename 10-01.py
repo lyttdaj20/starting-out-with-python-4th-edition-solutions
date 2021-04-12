d36129ba03fc23545c9f4de48e0557e2a0989b37
Dajan L <lyttdaj20@gmail.com>
2021-04-12 10:34:33 -0700

    Create 10-01.py

---
+++ 10-01.py
@@ -0,0 +1,41 @@
+
+# 10.1 Pet Class
+
+class Pet:
+    
+    def __init__(self, name, animal_type, age):
+        self.__name = name
+        self.__animal_type = animal_type
+        self.__age = age
+    
+    def set_name(self, name):
+        self.__name = name
+        
+    def set_animal_type(self, animal_type):
+        self.__animal_type = animal_type
+    
+    def set_age(self, age):
+        self.__age = age
+        
+    def get_name(self):
+        return self.__name
+    
+    def get_animal_type(self):
+        return self.__animal_type
+    
+    def get_age(self):
+        return self.__age
+    
+def main():
+    
+    name = input('Enter Name: ')
+    animal_type = input('Enter Animal Type: ')
+    age = int(input('Enter Age: '))
+    
+    my_pet = Pet(name, animal_type, age)
+    
+    print('Name:', my_pet.get_name())
+    print('Animal Type:', my_pet.get_animal_type())
+    print('Age:', my_pet.get_age())
+
+main()
