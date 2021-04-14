977d7ef9e9cc7815948904ccb20fd9e2d0dab872
Dajan L <lyttdaj20@gmail.com>
2021-04-14 16:52:49 -0700

    Create 08-12.py

---
+++ 08-12.py
@@ -0,0 +1,38 @@
+
+# 8.12 Word Separator
+
+def main():
+
+    sentence = input('Enter Sentence: ')
+    
+    seperated = ''
+    
+    word = ''
+
+    first = True
+
+    for ch in sentence:
+    
+        if ch.islower():
+            
+            word += ch
+    
+        else:
+            
+            seperated += word
+            
+            if word == '':
+            
+                seperated += ch
+                
+            else:
+                
+                seperated += ' ' + ch.lower()
+            
+            word = ''
+
+    seperated += word
+    
+    print(seperated)
+    
+main()
