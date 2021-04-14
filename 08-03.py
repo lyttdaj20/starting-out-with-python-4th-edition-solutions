718b5143ee2d6f52d445226897b0b2f822e2e13a
Dajan L <lyttdaj20@gmail.com>
2021-04-14 16:05:58 -0700

    Update 08-03.py

--- 08-03.py
+++ 08-03.py
@@ -5,33 +5,11 @@ def main():
 
     date = input('Enter Date (mm/dd/yyyy): ')
     
-    month = int(date[0:2])
+    month_names = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
+                   'September', 'October', 'November', 'December' ]
     
-    if month == 1:
-        month = 'January'
-    elif month == 2:
-        month = 'February'
-    elif month == 3:
-        month = 'March'
-    elif month == 4:
-        month = 'April'
-    elif month == 5:
-        month = 'May'
-    elif month == 6:
-        month = 'June'
-    elif month == 7:
-        month = 'July'
-    elif month == 8:
-        month = 'August'
-    elif month == 9:
-        month = 'September'
-    elif month == 10:
-        month = 'October'
-    elif month == 11:
-        month = 'November'
-    else:
-        month = 'December'
-        
+    month = month_names[int(date[0:2]) - 1]
+ 
     day = int(date[3:5])
     
     year = int(date[6:10])
