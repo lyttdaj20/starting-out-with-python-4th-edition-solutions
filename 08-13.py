5f7843d276282577155231ec9530fc6bb4c41218
Dajan L <lyttdaj20@gmail.com>
2021-04-14 16:40:53 -0700

    Create 08-13.py

---
+++ 08-13.py
@@ -0,0 +1,44 @@
+
+# 8.13 Pig Latin
+
+def main():
+    
+    sentence = input('Enter Sentence: ')
+    
+    translation = ''
+    
+    index = 0
+    word = ''
+
+    for ch in sentence:
+    
+        index += 1
+    
+        if ch != ' ':
+            
+            word += ch
+    
+        else:
+            
+            translation += pig_latin(word)
+            
+            word = ''
+
+    translation += pig_latin(word)
+    
+    print('Translation:', translation)
+    
+    
+def pig_latin(word):
+    
+    if len(word) == 1:
+        
+        return word + 'ay '
+    
+    else:
+        
+        return word[1:len(word)] + word[0] + 'ay '
+
+
+
+main()
