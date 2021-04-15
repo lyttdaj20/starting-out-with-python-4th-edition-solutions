83745dbc5f55439a0964d88c672a2b34d4030385
Dajan L <lyttdaj20@gmail.com>
2021-04-15 09:04:21 -0700

    Create 07-04.py

---
+++ 07-04.py
@@ -0,0 +1,35 @@
+
+# 7.4 Number Analysis Program
+
+def main():
+
+    numbers = []
+    
+    for i in range(20):
+        
+        numbers.append(int(input('Enter Number ' + str(i + 1) + ': ')))
+    
+    low = numbers[0]
+    high = numbers[0]
+    total = 0
+    
+    for i in numbers:
+        
+        if i < low:
+            
+            low = i
+            
+        if i > high:
+            
+            high = i
+        
+        total += i
+    
+    average = round(total / 20, 1)
+    
+    print('Low:', low)
+    print('High:', high)
+    print('Total:', total)
+    print('Average:', average)
+    
+main()
