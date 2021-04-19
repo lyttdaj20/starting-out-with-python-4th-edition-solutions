60fd92ef7552c5bf81cd89dfb9b5f77ea69a3ece
Dajan L <62118597+lyttdaj20@users.noreply.github.com>
2021-04-19 10:34:43 -0700

    Update 02-01.py

--- 02-01.py
+++ 02-01.py
@@ -2,6 +2,6 @@
 # 2.1 Personal Information
 
 print('Name: Dajan Lyttle')
-print('Address: 1900 Commerece St, Tacoma, WA, 98402')
+print('Address: 1900 Commerce St, Tacoma, WA, 98402')
 print('Number: (253) 692-4000')
 print('Major: Electrical Engineering')
